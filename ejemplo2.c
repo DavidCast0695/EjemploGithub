fd10938a8d153403935c67da613987052c33de9c
DavidCast <davidcast0695@gmail.com>
2018-03-28 15:23:30 -0600

    Agregamos nombre a ejemplo2.c

--- ejemplo2.c
+++ ejemplo2.c
@@ -5,10 +5,17 @@
 int main(int argc, char const *argv[])
 {
 	char nombre[50];
+	int edad;
+
 	printf("Introduce tu nombre: \n");
 	scanf("%s", nombre);
 
 	printf("Tu nombre es: %s\n", nombre);
+
+	printf("Introduce tu edad: \n");
+	scanf("%d", &edad);
+
+	printf("Tu edad es: %d\n", edad);
 	
 	return 0;
 }
