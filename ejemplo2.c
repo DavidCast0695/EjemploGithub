072c342be41673f70d9ce82882ee98a1386132ad
DavidCast <davidcast0695@gmail.com>
2018-03-28 14:49:10 -0600

    Primer commit Subiendo todo

---
+++ ejemplo2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	char nombre[50];
+	printf("Introduce tu nombre: \n");
+	scanf("%s", nombre);
+
+	printf("%s\n", nombre);
+	return 0;
+}
